60707c2cc6f95055b15a0688efc3132f19dc9957
Sean Flournoy <seanflournoy@new-host.home>
2024-11-03 21:38:04 -0500

    Updates to images & About page

---
+++ Testing Javascript/app.js
@@ -0,0 +1,12 @@
+console.log("Test")
+let go = "fast"
+
+if (go === "fast") {
+    console.log("You're really moving partner")
+}
+
+let rating = 2 
+
+if (rating === 2){
+    console.log("Get the swing step at a time")
+}
